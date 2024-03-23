007b922624770da353ff3a2f5bf29e0e29203966
Caio <cs1919328@gmail.com>
2024-03-22 22:47:05 -0300

    Atualizando 22/03/24

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
+import { RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Home from './routes/Home.jsx'
 import './style/index.css'
 
+const router = createBrowserRouter([ // Aqui eu crio as rotas a serem usadas
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: '/login',
+        element: <div>Ola, aqui vc faz o seu login</div>
+    }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App/>
+    <RouterProvider router={router} /> // aqui eu passo as totas definidas como argumento para Router provider
 )
